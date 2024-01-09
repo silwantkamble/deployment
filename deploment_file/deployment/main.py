0ffb2907c9b400985d97106ebf04cfa695901c42
silwantkamble <silwant.kamble@gmail.com>
2024-01-09 11:19:09 +0530

    deployment

--- deploment_file/deployment/main.py
+++ deploment_file/deployment/main.py
@@ -1,22 +1,20 @@
 #pip install flask 
-from flask import Flask
+from flask import Flask,render_template
 
 # initilise the app
 app=Flask(__name__)     #any name you can give
 
 @app.route("/")
 def home():
-    return "hello, how are you"
+    return render_template('main.html')
 
-
-
-@app.route("/blogs")
+@app.route("/input")
 def blog():
-    return "welcome to the home pages"
+    return render_template('input.html')
 
 @app.route("/contact_us")
 def gallary():
-    return "please contact on phone:87961626xx , and email:xyz@gmail.com"
+    return render_template('contact.html')
 
 #run the app
 app.run(debug=True)    #debug=True is for auto update of server
