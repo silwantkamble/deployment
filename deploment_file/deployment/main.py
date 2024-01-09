b9245291a2ede3033223908dd6e66697a009f34d
silwantkamble <silwant.kamble@gmail.com>
2024-01-09 09:53:31 +0530

    flask

--- deploment_file/deployment/main.py
+++ deploment_file/deployment/main.py
@@ -7,5 +7,16 @@ app=Flask(__name__)     #any name you can give
 @app.route("/")
 def home():
     return "hello, how are you"
+
+
+
+@app.route("/blogs")
+def blog():
+    return "welcome to the home pages"
+
+@app.route("/contact_us")
+def gallary():
+    return "please contact on phone:87961626xx , and email:xyz@gmail.com"
+
 #run the app
-app.run()
+app.run(debug=True)    #debug=True is for auto update of server
